748952fb051390e7c473847ed1582e99287ad514
yamamotoeigo <otokoume12345@gmail.com>
2025-02-16 02:06:00 +0900

    fix

--- libs/client.ts
+++ libs/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
-  serviceDomain: process.env.MICROCMS_APP_DOMAIN || "",
-  apiKey: process.env.MICROCMS_APP_KEY || "",
+  serviceDomain: process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN || "",
+  apiKey: process.env.NEXT_PUBLIC_MICROCMS_API_KEY || "",
 });
