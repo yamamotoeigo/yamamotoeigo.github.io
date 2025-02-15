5494ba92d2d0774680130a49b29c18a9309fd29e
yamamotoeigo <otokoume12345@gmail.com>
2025-02-16 01:26:40 +0900

    fix

--- libs/client.ts
+++ libs/client.ts
@@ -1,7 +1,12 @@
 import { createClient } from 'microcms-js-sdk';
 
-// microCMSのクライアントを作成
+const serviceDomain = process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN;
+const apiKey = process.env.NEXT_PUBLIC_MICROCMS_API_KEY;
+if (!serviceDomain || !apiKey) {
+  throw new Error('microCMS の serviceDomain および apiKey は必須です。環境変数を確認してください。');
+}
+
 export const client = createClient({
-  serviceDomain: process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN!, // microCMSのサービスドメイン
-  apiKey: process.env.NEXT_PUBLIC_MICROCMS_API_KEY!, // APIキー
+  serviceDomain,
+  apiKey,
 });
